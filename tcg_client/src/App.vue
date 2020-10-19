f0c45b50f38150fb5909440deb0a1de11463c91d
yangfan <yangfan_94@outlook.com>
2020-10-19 21:49:09 +0800

    client - generator form coded

--- tcg_client/src/App.vue
+++ tcg_client/src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <a-config-provider :locale="locale">
+
+    <a-layout id="app-container">
+      <a-layout-header class="header">
+        <div class="logo"/>
+        <a-menu theme="dark" mode="horizontal" v-model:selectedKeys="menuSelectedKeys" class="menu">
+          <a-menu-item key="1" @click="pushRoute('code-list')">
+            Trial Code List
+          </a-menu-item>
+          <a-menu-item key="2" @click="pushRoute('code-gen')">
+            Trial Code Generator
+          </a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <div class="router-viewer">
+          <router-view/>
+        </div>
+      </a-layout-content>
+      <a-layout-footer class="footer" style="text-align: center">
+        HENGRUI MEDICINE ©2020
+      </a-layout-footer>
+    </a-layout>
+  </a-config-provider>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import enGB from 'ant-design-vue/es/locale/en_GB';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'moment/locale/en-gb';
+moment.locale('en-gb');
+export default {
+  data () {
+    return {
+      locale: enGB,
+      menuSelectedKeys: ['2'],
+    };
+  },
+  methods: {
+    pushRoute: function (routeName) {
+      this.$router.push({
+        name: routeName,
+      })
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app-container .logo {
+  width: 120px;
+  height: 50px;
+  background: url('~@/../public/logo2.png') no-repeat;
+  background-size: 100% 100%;
+  margin: 7px 28px 7px 0;
+  float: left;
+}
+#app-container .menu {
+  line-height: 64px;
+}
+
+@media screen and (min-width: 768px) {
+  #app-container .content {
+    padding: 25px 50px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  #app-container .content {
+    padding: 25px 15px;
+  }
+}
+
+#app-container .router-viewer {
+  background-color: #fff;
+}
+#app-container .footer {
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ tcg_client/src/views/List.vue
@@ -0,0 +1,81 @@
+<template>
+  <a-row>
+
+    <a-col :lg="6">
+      <a-form :model="codeGeneratorForm" :label-col="labelCol" :wrapper-col="wrapperColContent">
+        <a-form-item label="Compound Code">
+          <a-input v-model:value="codeGeneratorForm.compoundCode" placeholder="Please input the compound code" type="text"/>
+        </a-form-item>
+        <a-form-item label="Trial Phase">
+          <a-select v-model:value="codeGeneratorForm.trialPhase" placeholder="Please select a trial phase">
+            <a-select-option value="p1">
+              Phase I
+            </a-select-option>
+            <a-select-option value="p2">
+              Phase II
+            </a-select-option>
+            <a-select-option value="p3">
+              Phase III
+            </a-select-option>
+            <a-select-option value="p4">
+              Phase IV
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item label="Date of Generation">
+          <a-date-picker v-model:value="codeGeneratorForm.dateOfGeneration" type="date"/>
+        </a-form-item>
+        <a-form-item label="Enable Country Code">
+          <a-switch v-model:checked="codeGeneratorForm.countryCodeFlag"/>
+        </a-form-item>
+        <a-form-item label="Country Code" v-if="codeGeneratorForm.countryCodeFlag === true">
+          <a-input v-model:value="codeGeneratorForm.countryCode" type="text"/>
+        </a-form-item>
+        <a-form-item :wrapper-col="wrapperColButton" class="button-container">
+          <a-button type="primary" @click="generateTrialCode" v-if="!waiting">
+            Generate
+          </a-button>
+          <a-spin v-else/>
+        </a-form-item>
+      </a-form>
+    </a-col>
+
+    <a-col :lg="18">
+      222222222222222222222222222222222222222222222222222222222222222
+    </a-col>
+
+  </a-row>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data() {
+    return {
+      labelCol: { span: 6 },
+      wrapperColContent: { span: 18 },
+      wrapperColButton: { span: 24 },
+      codeGeneratorForm: {
+        compoundCode: '',
+        trialPhase: undefined,
+        dateOfGeneration: moment(new Date(), 'YYYY-MM-DD'),
+        countryCodeFlag: false,
+        countryCode: '',
+      },
+    };
+  },
+
+}
+</script>
+
+<style scoped>
+#code-list .sider {
+  width: 200px;
+  background-color: #fff;
+}
+</style>
